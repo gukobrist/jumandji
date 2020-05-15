a271e2263d5e779153e428bc669113ecafee3dee
Romashov Aleksey <amromashov@gmail.com>
2020-05-15 14:34:20 +0300

    last commit

--- djoomandji/jobsearch/models.py
+++ djoomandji/jobsearch/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Company(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
@@ -11,7 +10,6 @@ class Company(models.Model):
     def __str__(self):
         return self.name
 
-
 class Specialty(models.Model):
     code = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
@@ -19,7 +17,6 @@ class Specialty(models.Model):
 
     def __str__(self):
         return self.title
-
 
 class Vacancy(models.Model):
     title = models.CharField(max_length=100)
